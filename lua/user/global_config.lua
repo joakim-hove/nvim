3377b36679ce371b42e9148c6eecdeac8340b716
Joakim Hove <joakim.hove@gmail.com>
2024-11-14 16:26:37 +0100

    Update binding to neogit

--- lua/user/global_config.lua
+++ lua/user/global_config.lua
@@ -1,6 +1,9 @@
--- Bind 'G' to start neogit
-vim.keymap.set('n', 'G', ':Neogit<CR>')
+-- Bind 'ø' to start neogit
+vim.keymap.set('n', 'ø', ':Neogit<CR>')
 
 
 -- Bind '-' to vertically split window
 vim.keymap.set('n', '-', ":split<CR>")
+
+-- Use relative linenumbers
+vim.wo.relativenumber = true

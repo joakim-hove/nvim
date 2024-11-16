a60cc0b29707914b02bfc69360fe4e5c3b7c392a
Joakim Hove <joakim.hove@gmail.com>
2024-11-16 12:14:25 +0100

    Add mapping to telecope current buffer path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>gG", "<Cmd>Neogit<CR>", { desc = "Open neogit" })
+
+local builtin = require("telescope.builtin")
+local utils = require("telescope.utils")
+vim.keymap.set("n", "<leader>fd", function()
+  builtin.find_files({ cwd = utils.buffer_dir() })
+end, { desc = "Open telescope in directory of current buffer" })
